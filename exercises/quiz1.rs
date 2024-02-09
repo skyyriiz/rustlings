5bf139044c61ee1cc48d980f70f46900e30ad396
Mikey <leblancmike525252@gmail.com>
2024-02-09 12:24:20 +0100

    done

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
+
+fn calculate_price_of_apples(num: i32) -> i32{
+    if num > 40 {
+        num
+    } else {
+        num * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
@@ -31,3 +37,4 @@ fn verify_test() {
     assert_eq!(41, price3);
     assert_eq!(65, price4);
 }
+
